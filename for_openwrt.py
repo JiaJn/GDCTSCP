3f95c69b91b2810b31505082da93a1af0eba3681
mynuolr <mynuolr@foxmail.com>
2015-09-14 16:41:39 +0800

    Merge branch 'master' into Stable-1.0

--- for_openwrt.py
+++ for_openwrt.py
@@ -8,11 +8,19 @@ import urllib2
 import time
 import json
 ISOTIMEFORMAT='%Y-%m-%d %X'
+<<<<<<< HEAD
 client=“0.0.0.0”                #client ip
 nasip=“0.0.0.0”                  #net auth ip
 user=“user”                     #user
 password="123456"                      #password
 mac=“FF-FF-FF-FF-FF-FF”               #mac address
+=======
+client="0.0.0.0"                #client ip
+nasip=“0.0.0.0”                 #net auth ip
+user="user"                     #user
+password="password"                      #password
+mac="FF-FF-FF-FF-FF-FF"               #mac address eg.FF-FF-FF-FF-FF-FF
+>>>>>>> master
 wifi="4060"
 url="http://enet.10000.gd.cn:10001/client/"
 login = url + "login"
@@ -122,26 +130,25 @@ def kepp_xintiao():
             if(str2=="0"):
                 time.sleep(120)
                 continue
-            elif(str2=="-1"):
+            elif(str2=="1"):
                 loginl()
                 continue
             else:
                 continue
 while 1:
     try:
-        rn=urllib2.urlopen(testurl)
-        cc=rn .geturl()
         while 1 :
+            rn=urllib2.urlopen(testurl)
+            cc=rn .geturl()
             if (cc==testurl):
                 r=xintiao()
                 print Now_time()+r
                 if (r!="x"):
                     str2=r.split('\"')[3]
-                    if (str2=="1"):
-                        exit()
-                    else:
+                    if (str2=="0"):
                         break
             loginl()
+            
         time.sleep(60)
         kepp_xintiao()
     except urllib2.HTTPError, e:
